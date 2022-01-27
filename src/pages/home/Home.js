f19d24bbaea9eed1e0569ed66a2d5f4300a6066b
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-01-26 22:07:13 -0500

    Transaction Form

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,16 @@
-import React from "react";
-
 // Styles
 import styles from "./Home.module.css";
 
+// Components
+import TransactionForm from "./TransactionForm";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>transaction list</div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
+  );
 }
